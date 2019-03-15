061b29ec60586d32f6e3b8cb938e60c83272077a
ChangYuCheng <1434389213@qq.com>
2019-03-15 16:58:24 +0800

    empty

--- catalog/study/PHP/SelfTest.php
+++ catalog/study/PHP/SelfTest.php
@@ -14,12 +14,19 @@ class SelfTest implements Iterator{
     private static $_self;  // 如果需要扩展，$_self为实体
 
     protected $file;        // 试题位置
-    public $test = [];      // 试题内容
+    protected $test = [];      // 试题内容
 
     private $end;           // 结束位置
     private $iterator;      // 迭代记录
     private $input;         // 检测输入
-    private $reduce;        // 成绩汇总
+    private $reduce = [
+        'success'       => 0,   // 正确个数
+        'error'         => 0,   // 错误个数
+        'jump'          => 0,   // 跳过个数
+        'num'           => 0,   // 已答个数
+        'remnant'       => 0,   // 剩余个数
+        's'             => 0,   // 正确率(所有)
+    ];                      // 成绩汇总
 
     /**
      * SelfTest constructor.
@@ -53,11 +60,21 @@ class SelfTest implements Iterator{
             // 删除标题部分
             array_shift($this ->test);
             if (!count($this ->test)) throw new Exception('试题文件解析失败');
-            if ($rand) {
+            if ($rand) { // 顺序随机打乱
                 foreach ($this ->test as $k =>$v) {
                     shuffle($this ->test[$k]);
                 }
+                $temp = [];
+                foreach ($this ->test as $k =>$v) {
+                    $temp[] = [$k =>$v];
+                }
+                shuffle($temp);
+                $this ->test = null;
+                foreach ($temp as $v) {
+                    $this ->test[key($v)] = current($v);
+                }
             }
+            foreach ($this as $v) {}
         }catch (Exception $e) {
             exit("构造出错，因为{$e ->getMessage()}" . PHP_EOL);
         }
@@ -73,19 +90,57 @@ class SelfTest implements Iterator{
         return self::$_self ->test;
     }
 
+    public function fliter() {
+        $fliter = null;
+        foreach (array_keys($this ->test) as $k =>$v) {
+            $fliter .= "{$k}. {$v} ";
+        }
+        $fliter = trim($fliter);
+        fwrite(STDOUT, "当前有：{$fliter}，请按照位置筛选，为空则为全部(多个使用\",\"隔开)" . PHP_EOL);
+        $fliter = null;
+        fscanf(STDIN, '%s', $fliter);
+        if (($fliter = trim($fliter)) !== ''){
+            $fliter = explode(',', trim($fliter, ','));
+            $temp = [];
+            foreach($this ->test as $k =>$v) {
+                $temp[] = [$k =>$v];
+            }
+            // 获取差集，删除，
+            $fliter = array_diff(array_keys($temp), $fliter);
+            if (count($fliter) != count($temp)) {
+                foreach ($fliter as $d) {
+                    unset($temp[$d]);
+                }
+            }
+            $this ->test = null;
+            foreach($temp as $v) {
+                $this ->test[key($v)] = current($v);
+            }
+        }
+    }
+
     public function rewind() {
-        reset($this ->test);
+        $this ->fliter();
+
+        reset($this ->test); // 重置当前位置
         $this ->iterator = [key($this ->test), 0];                                // 记录当前位置
         $this ->end = array_keys($this ->test)[count(array_keys($this ->test)) - 1];// 获取最后一个章节位置
+        $this ->reduce['remnant'] = array_reduce($this ->test, function ($carry, $item) { // 汇总所有个数
+            return $carry + count($item);
+        }, 0);
     }
 
     public function valid() {
-//        fwrite(STDOUT, str_repeat(PHP_EOL, 100)); // 清屏
-        system('clear');
+        // 清屏
+        if (!isset($_SERVER))                           fwrite($this ->output);
+        elseif (!array_key_exists('SHELL', $_SERVER))   throw new Exception('无法在非shell环境下运行');
+        else                                            system(strtolower(PHP_OS) == 'linux'?'clear':'cls');
+
+        fwrite(STDOUT, "当前分类为:{$this ->iterator[0]}\t正确:{$this ->reduce['success']}个\t错误:{$this ->reduce['error']}个\t跳过:{$this ->reduce['jump']}次\t已答:{$this ->reduce['num']}个\t剩余:{$this ->reduce['remnant']}个\t正确率:{$this ->reduce['s']}%" . PHP_EOL);
         if ($this ->iterator[0] == $this ->end && $this ->iterator[$this ->end] == count($this ->iterator[$this ->end]) - 1) {
-            fwrite(STDOUT, $this ->reduce . PHP_EOL);
+            fwrite(STDOUT, '--测试完毕--');
         }else {
-            fwrite(STDOUT, $this ->input . PHP_EOL);
+            fwrite(STDOUT, wordwrap($this ->input ?? __CLASS__) . PHP_EOL);
             $this ->input = null;
             return true;
         }
@@ -95,18 +150,29 @@ class SelfTest implements Iterator{
         $current = $this ->test[$this ->iterator[0]][$this ->iterator[1]];
         $success = 0;
         similar_text($this ->input, $current[0], $success);
-        if (!$this ->input) {
-            $this ->input = '弃权';
+        if (!$this ->input) { // 跳过
+            $this ->reduce['jump'] ++;
+            $this ->input = "跳过:{$current[0]}" . PHP_EOL;
         }elseif ($this ->input == $current[0]) {
             $this ->iterator[1] ++; // 回答正确n(*≧▽≦*)n， 进入下一道试题
             if ($this ->iterator[1] >= count($this ->test[$this ->iterator[0]]) - 1) {
                 next($this ->test);
                 $this ->iterator[0] = key($this ->test);
             }
-            $this ->input = "\t上次回答：函数{$current[0]}：{$current[2]}" . PHP_EOL;
+            // 正确个数记录
+            $this ->reduce['success'] ++;
+            // 剩余个数记录
+            $this ->reduce['remnant'] --;
+            $this ->input = "{$current[0]}：{$current[2]}" . PHP_EOL;
         }else {
+            // 错误个数记录
+            $this ->reduce['error'] ++;
             $this ->input = '回答错误，该函数的说明为：' . $current[2] . PHP_EOL . "\t" . '您的答案与正确答案的相似度为' . round($success, 2) . '，请再次回答';
         }
+        // 已答个数
+        $this ->reduce['num'] ++;
+        // 正确率，保留两位小数
+        $this ->reduce['s'] = round($this ->reduce['success'] / $this ->reduce['num'] * 100, 2);
     }
 
     public function key() {
@@ -114,9 +180,9 @@ class SelfTest implements Iterator{
     }
 
     public function current() {
-        fwrite(STDOUT, '在' . $this ->iterator[0] . '中，' . $this ->test[$this ->iterator[0]][$this ->iterator[1]][1] . '的函数为：' . PHP_EOL);
+        // fwrite(STDOUT, '在' . $this ->iterator[0] . '中，' . $this ->test[$this ->iterator[0]][$this ->iterator[1]][1] . '的函数为：' . PHP_EOL);
+        fwrite(STDOUT, $this ->test[$this ->iterator[0]][$this ->iterator[1]][1] . '的函数为：' . PHP_EOL);
         fscanf(STDIN, '%s', $this ->input); // 接收输入
     }
 }
-
-foreach(new SelfTest('./function.md') as $v) {}
+new SelfTest('./function.md');
