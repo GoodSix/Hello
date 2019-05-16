83e1b54c461d5f7c0ca29fa08c20c1c0479a0065
GoodSix <1434389213@qq.com>
2019-05-16 08:21:17 +0800

    哈哈

--- SelfTest3.0/front/index.php
+++ SelfTest3.0/front/index.php
@@ -1,4 +1,3 @@
 <?php
-//if(!array_key_exists('HTTP_X_REQUESTED_WITH', $_SERVER) && !isset($_SERVER["HTTP_X_REQUESTED_WITH"]) && !strtolower($_SERVER["HTTP_X_REQUESTED_WITH"])=="xmlhttprequest") die;
-
+// 果然这样会更好
 include dirname(__DIR__) . '/index.php';
